e00b0a2a74a32d9a27fc7b8db158818dc5b4f593
TimZ <tiomsity@gmail.com>
2022-10-04 12:27:48 -0500

    added useHover

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,37 @@
 import "./App.css";
+import { useState, useRef, useEffect } from "react";
+
+const useHover = () => {
+  const [hovered, setHovered] = useState(false);
+  const ref = useRef(null);
+
+  const handleMouseOver = () => setHovered(true);
+  const handleMouseOut = () => setHovered(false);
+
+  useEffect(() => {
+    const node = ref.current;
+    if (!node) return;
+    node.addEventListener("mouseover", handleMouseOver);
+    node.addEventListener("mouseout", handleMouseOut);
+    return () => {
+      node.removeEventListener("mouseover", handleMouseOver);
+      node.removeEventListener("mouseout", handleMouseOut);
+    };
+  }, []);
+
+  return [ref, hovered];
+};
 
 function App() {
-  return <div className="App"></div>;
+  const [hoverRef, hovered] = useHover();
+
+  return (
+    <div className="App">
+      <h1 ref={hoverRef} className={!hovered ? "" : "hover-text-red"}>
+        Hover over me and you will see me turn red.
+      </h1>
+    </div>
+  );
 }
 
 export default App;
