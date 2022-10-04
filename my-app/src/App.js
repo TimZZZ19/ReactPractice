606004bbff603e852b270b8eabe586ccdcb74163
TimZ <tiomsity@gmail.com>
2022-10-04 11:55:10 -0500

    added DetectHover

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
+import { useState } from "react";
+
+const DetectHover = ({ render }) => {
+  const [hovered, setHovered] = useState(false);
+  return (
+    <div
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      {render(hovered)}
+    </div>
+  );
+};
 
 function App() {
-  return <div className="App"></div>;
+  const render = (hovered) => (
+    <div className="App">
+      <h1 className={hovered && "hover-text-red"}>
+        Hover over me and you will see me turn red.
+      </h1>
+    </div>
+  );
+
+  return <DetectHover render={render} />;
 }
 
 export default App;
