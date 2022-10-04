7e7a2009937bd1c203446699a776e05c458f6935
TimZ <tiomsity@gmail.com>
2022-10-04 11:22:46 -0500

    added detectHover on the HOC branch

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,28 @@
-import "./App.css";
+import styles from "./App.module.css";
+import { useState } from "react";
 
-function App() {
-  return <div className="App"></div>;
+const detectHover = (Component) => {
+  return () => {
+    const [hovered, setHovered] = useState(false);
+    return (
+      <div
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+      >
+        <Component hovered={hovered} />
+      </div>
+    );
+  };
+};
+
+function App(props) {
+  return (
+    <div className={styles.App}>
+      <h1 className={props.hovered && styles["hover-text-red"]}>
+        Hover over me and you will see me turn red.
+      </h1>
+    </div>
+  );
 }
 
-export default App;
+export default detectHover(App);
